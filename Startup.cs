187ec7fbe725ebdbeeffd976fa8ecea0f3a2e2c5
beniz-el <59539157+beniz-el@users.noreply.github.com>
2020-11-18 23:06:08 +0100

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -23,10 +23,13 @@ namespace MvcMovieCore
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllersWithViews();
-        }
+      public void ConfigureServices(IServiceCollection services)
+{
+    services.AddControllersWithViews();
+
+    services.AddDbContext<MvcMovieContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("MvcMovieContext")));
+}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
