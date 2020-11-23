8eea8c75e14eb501b354e142af3bc39657ae60fc
beniz-el <59539157+beniz-el@users.noreply.github.com>
2020-11-23 21:34:29 +0100

    Update MoviesController.cs

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -27,7 +27,7 @@ namespace MvcMovieCore.Controllers
 
         // GET: Movies/Details/5
         // GET: Movies/Details/5
-        public async Task<IActionResult> Details(int? id)
+         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
             {
